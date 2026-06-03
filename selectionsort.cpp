2e8035ab7a2211efd7809016de77c69636c023bb
Sumit Kumar Sah <sksumitkumar2618@gmail.com>
2026-06-03 10:20:58 +0530

    Implement bubble sort and selection sort algorithms
    Added bubble sort and selection sort functions to the file.

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,32 +1,35 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-// void bubbleSorting(int arr[],int n){
-//     for(int i =0; i<n-1;i++){
-// for(int j=0;j<=n-1-i;j++){
-// if(arr[j]>arr[j+1]){
-//   swap(arr[j],arr[j+1]);
-// }
-// } 
 
-// }
-// }
+void bubbleSorting(int arr[],int n){
+    for(int i =0; i<n-1;i++){
+for(int j=0;j<=n-1-i;j++)
+if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+}
+}
+
+//elements get sorted from left 
+void selectionSort(int arr[],int n){
+    for(int i =0; i<n-1;i++){   // i for accessing the array
+int minindex = i;
+for(int j =i+1;j<n;j++)  // j for accesssing the  array
+if(arr[j]<arr[minindex]) minindex = j;
+swap(arr[i], arr[minindex]);   
+}
+}
 
 void insertionSorting(int arr[],int n){
     for(int i =1; i<=n-1;i++){
-        int x = arr[i];
         int j=i-1;
 for(;j>=0;j--){
-if(arr[j]>x){
- arr[j+1]=arr[j];
-}
-else{
-    break;
-}
+if(arr[j]>arr[i] ) arr[j+1]=arr[j];
+else break;
 } 
 arr[j+1]=x;
 }
 }
+
 int main(){
     int arr[5]= {90,-45,-7,78,4};
    insertionSorting(arr,5) ;
